8e0fb8a9676ba64604f3cdb03dfd161fb5d88d30
krissy <krissy@cirno.krissy.club>
2023-12-17 01:22:48 -0500

    most functionality implemented

---
+++ bot.rb
@@ -0,0 +1,24 @@
+require 'discordrb'
+require 'sqlite3'
+
+bot = Discordrb::Bot.new token: File.read('../token.txt').strip
+db = SQLite3::Database.open 'posts.db'
+
+bot.message do |message|
+  if message.content[0] == '+'
+    lines = message.content.split $/
+    tags = lines[1]
+    content = lines[2..].join $/
+    date = Time.now.to_i.to_s
+    db.execute "INSERT INTO tbl1 VALUES (?, ?, ?)", content, tags, date
+    message.respond date
+  end
+  if message.content[0] == '-'
+    lines = message.content.split $/
+    date = lines[1]
+    db.execute "DELETE FROM tbl1 WHERE date=?", date
+    message.respond date
+  end
+end
+
+bot.run
--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css.css">
+<title>hepatiki</title>
 </head>
 
 <?php
+date_default_timezone_set('EST');
 echo "<a href='/'><div class=term style='display:flex;justify-content:center'><pre>
    __                   __   _  __    _ 
   / /  ___  ___  ___ _ / /_ (_)/ /__ (_)
@@ -16,35 +18,44 @@ A small personal blog / wiki</pre></div></a>";
 $db = new PDO('sqlite:posts.db');
 $query = $_SERVER['QUERY_STRING'];
 
-if (strlen($query) > 0){
-    $stmt = $db->prepare('SELECT * FROM tbl1 WHERE tags LIKE :query');
-    $stmt->bindValue(':query', "%$query%");
-    $stmt->execute();
-    $res = $stmt->fetchAll();
-    foreach ($res as $res) {
-        $tags = explode(" ", $res['tags']);
-        $date = date("d M Y", $res['date']);
-        echo "<div class=term style='margin-top:5px'>
-        <div style='float:right'>{$res['date']}</div>
-        <div style='float:left'>tags: ";
-        foreach ($tags as $tag) {
-            echo "<a href='/?{$tag}'>{$tag}</a> ";
-        }
-        echo "</div><br>{$date}<br>{$res['content']}</div>";
+$res = select_with_query($query, $db);
+foreach ($res as $res) {
+    $tags = explode(" ", $res['tags']);
+    $date = date("d M Y H:i", $res['date']);
+    $parsed_text = parse_text($res['content']);
+    echo "<div class=term style='margin-top:5px'>
+    <div style='float:right'>{$res['date']}</div>
+    <div style='float:left'>tags: ";
+    foreach ($tags as $tag) {
+        echo "<a href='/?{$tag}'>{$tag}</a> ";
     }
-} else {
-    $stmt = $db->prepare('SELECT * FROM tbl1');
-    $stmt->execute();
-    $res = $stmt->fetchAll();
-    foreach ($res as $res) {
-        $tags = explode(" ", $res['tags']);
-        $date = date("d M Y", $res['date']);
-        echo "<div class=term style='margin-top:5px'>
-        <div style='float:right'>{$res['date']}</div>
-        <div style='float:left'>tags: ";
-        foreach ($tags as $tag) {
-            echo "<a href='/?{$tag}'>{$tag}</a> ";
-        }
-        echo "</div><br>{$date}<br>{$res['content']}</div>";
+    echo "</div><br>{$date}<br>{$parsed_text}</div>";
+}
+
+function select_with_query($query, $db)
+{
+    if (strlen($query) > 0){
+        $stmt = $db->prepare('SELECT * FROM tbl1 WHERE tags LIKE :query ORDER BY date DESC');
+        $stmt->bindValue(':query', "%$query%");
+    } else {
+        $stmt = $db->prepare('SELECT * FROM tbl1 ORDER BY date DESC');
     }
+    $stmt->execute();
+    return $stmt->fetchAll();
+}
+
+function parse_text($content)
+{
+    $textarr = explode("\n", $content);
+    for ($i = 0; $i < sizeof($textarr); $i++) {
+        $str = $textarr[$i];
+        if (strcmp($str[0], ">") == 0) {
+            $rest = substr($str, 1);
+            $textarr[$i] = "<span class='greentext'>&gt;{$rest}</span>";
+        } else if (strcmp($str[0], "<") == 0) {
+            $rest = substr($str, 1);
+            $textarr[$i] = "<span class='redtext'>&lt;{$rest}</span>";
+        }
+    }
+    return join("<br>", $textarr);
 }
